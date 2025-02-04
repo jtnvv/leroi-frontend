d081c1c07d353c6fa6698c236fe0a8b069f46307
Sebls <sebas.lopezsilva147@gmail.com>
2025-02-04 10:20:19 -0500

    Formato

--- src/App.jsx
+++ src/App.jsx
@@ -18,44 +18,44 @@ import Pricing from './pages/Pricing';
 function App() {
   return (
     <>
-    <Toaster 
-      position="top-right"
-      toastOptions={{
-        duration: 4000,
-        style: {
-          background: '#1a1a1a',
-          color: '#fff',
-          border: '1px solid #835bfc'
-        },
-        success: {
-          iconTheme: {
-            primary: '#835bfc',
-            secondary: '#fff',
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          duration: 4000,
+          style: {
+            background: '#1a1a1a',
+            color: '#fff',
+            border: '1px solid #835bfc'
           },
-        },
-        error: {
-          iconTheme: {
-            primary: '#ff5757',
-            secondary: '#fff',
+          success: {
+            iconTheme: {
+              primary: '#835bfc',
+              secondary: '#fff',
+            },
           },
-        }
-      }}
-    />
-    <Router>
-      <div className="App">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/roadmap" element={<ProtectedRoute><Roadmap /></ProtectedRoute>} />
-          <Route path="/reset-password" element={<ResetPassword />} />
-          <Route path="/about" element={<About/>} />
-          <Route path="/pricing" element={<Pricing />} />
-        </Routes>
-      </div>
-    </Router>
+          error: {
+            iconTheme: {
+              primary: '#ff5757',
+              secondary: '#fff',
+            },
+          }
+        }}
+      />
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/roadmap" element={<ProtectedRoute><Roadmap /></ProtectedRoute>} />
+            <Route path="/reset-password" element={<ResetPassword />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/pricing" element={<Pricing />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }

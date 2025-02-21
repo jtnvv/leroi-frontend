818f77a34c4f144474181e2f63f2307e04a1a7c5
amateusl <amateusl@unal.edu.co>
2025-02-21 16:51:16 -0500

    Merge con front actualizado / funcionalidad guardar

--- src/pages/GeneratedRoadmap.jsx
+++ src/pages/GeneratedRoadmap.jsx
@@ -1,6 +1,8 @@
 import { useLocation, useNavigate } from 'react-router-dom';
 import { useState, useRef } from 'react';
 import ReactFlow, { Background, Controls, ControlButton } from 'react-flow-renderer';
+
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDownload, faTimes, faSearchPlus, faSearchMinus, faExpand } from '@fortawesome/free-solid-svg-icons';
 import { faLink } from '@fortawesome/free-solid-svg-icons'; 
@@ -26,8 +28,8 @@ function GeneratedRoadmap() {
   };
 
   const handleNewRoadmap = () => {
-    const topicState = relatedTopicsModal ? {relatedTopics} : {};
-    navigate('/roadmap', {state: {topicState}});
+    const topicState = relatedTopicsModal ? { relatedTopics } : {};
+    navigate('/roadmap', { state: { topicState } });
   };
 
   const closeModal = () => {
@@ -38,90 +40,43 @@ function GeneratedRoadmap() {
   const edges = [];
 
   let idCounter = 0;
-  const levelOffset = 600; 
-  const nodeWidth = 500; 
+  const levelOffset = 500;
+  const nodeWidth = 400;
 
-  Object.keys(roadmapTopics).forEach((topicKey, topicIndex) => {
-    const topicNode = {
-      id: `topic-${idCounter++}`,
-      data: { label: topicKey, color: '#ffca00' },
-      position: { x: 0, y: 500},
-      type: 'custom',
-    };
-    nodes.push(topicNode);
-
-    const topic = roadmapTopics[topicKey];
-    Object.keys(topic).forEach((subtopicKey, subtopicIndex) => {
-      const subtopicNode = {
-        id: `subtopic-${idCounter++}`,
-        data: { label: subtopicKey, color: '#96E6B3' },
-        position: { x: nodeWidth, y: topicIndex * levelOffset + subtopicIndex * levelOffset / 2 },
+  if (roadmapTopics) {
+    Object.keys(roadmapTopics).forEach((topicKey, topicIndex) => {
+      const topicNode = {
+        id: `topic-${idCounter++}`,
+        data: { label: topicKey, color: '#ffca00' },
+        position: { x: 0, y: 500 },
         type: 'custom',
       };
-      nodes.push(subtopicNode);
-      edges.push({ id: `e-${topicNode.id}-${subtopicNode.id}`, source: topicNode.id, target: subtopicNode.id });
+      nodes.push(topicNode);
 
-      topic[subtopicKey].forEach((subSubtopic, index) => {
-        const subSubtopicNode = {
-          id: `subSubtopic-${idCounter++}`,
-          data: { label: subSubtopic, color: '#FF92E6' },
-          position: { x: nodeWidth * 2, y: topicIndex * levelOffset + subtopicIndex * levelOffset / 2 + index * 50 },
+      const topic = roadmapTopics[topicKey];
+      Object.keys(topic).forEach((subtopicKey, subtopicIndex) => {
+        const subtopicNode = {
+          id: `subtopic-${idCounter++}`,
+          data: { label: subtopicKey, color: '#96E6B3' },
+          position: { x: nodeWidth, y: topicIndex * levelOffset + subtopicIndex * levelOffset / 2 },
           type: 'custom',
-
         };
-        nodes.push(subSubtopicNode);
-        edges.push({ id: `e-${subtopicNode.id}-${subSubtopicNode.id}`, source: subtopicNode.id, target: subSubtopicNode.id });
+        nodes.push(subtopicNode);
+        edges.push({ id: `e-${topicNode.id}-${subtopicNode.id}`, source: topicNode.id, target: subtopicNode.id });
+
+        topic[subtopicKey].forEach((subSubtopic, index) => {
+          const subSubtopicNode = {
+            id: `subSubtopic-${idCounter++}`,
+            data: { label: subSubtopic, color: '#FF92E6' },
+            position: { x: nodeWidth * 2, y: topicIndex * levelOffset + subtopicIndex * levelOffset / 2 + index * 50 },
+            type: 'custom',
+          };
+          nodes.push(subSubtopicNode);
+          edges.push({ id: `e-${subtopicNode.id}-${subSubtopicNode.id}`, source: subtopicNode.id, target: subSubtopicNode.id });
+        });
       });
     });
-  });
-
-  const captureRoadmap = async () => {
-    const roadmapElement = document.getElementById('roadmap-container');
-    if (!roadmapElement) return;
-
-    try {
-      const canvas = await html2canvas(roadmapElement, { scale: 2 });
-      const imgData = canvas.toDataURL('image/png');
-      await saveImageToDB(imgData);
-    } catch (error) {
-      console.error('Error al capturar la imagen:', error);
-    }
-  };
-
-  const saveImageToDB = async (base64Image) => {
-    const authToken = localStorage.getItem("token");
-
-    if (!roadmapTopics || Object.keys(roadmapTopics).length === 0) {
-      console.error("No hay datos del roadmap para guardar.");
-      return;
-    }
-
-    const topic = Object.keys(roadmapTopics)[0]; 
-    const roadmapData = JSON.stringify(roadmapTopics); 
-
-    try {
-      const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/save-roadmap-image`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${authToken}`,
-        },
-        body: JSON.stringify({ 
-          topic,  
-          roadmap_data: roadmapData,  
-          image_base64: base64Image  
-        }),
-      });
-
-      if (!response.ok) {
-        throw new Error('Error al guardar la imagen en la base de datos');
-      }
-
-      console.log('Roadmap guardado correctamente');
-    } catch (error) {
-      console.error('Error al enviar el roadmap al backend:', error);
-    }
-};
+  }
 
   const handleDownload = async (format) => {
     const roadmapElement = roadmapRef.current;
@@ -199,21 +154,8 @@ function GeneratedRoadmap() {
 
   return (
     <div className="generated-roadmap-container">
-      <button onClick={captureRoadmap} className="capture-button">
-        Guardar Roadmap
-      </button>
-
-      <div id="roadmap-container" className="react-flow-container">
-        <ReactFlow nodes={nodes} edges={edges} nodeTypes={{ custom: CustomNode }} fitView>
-          <Background />
-          <Controls />
-        </ReactFlow>
-      </div>
       {roadmapTopics && (
         <>
-          <button className="icon-button" onClick={() => setShowDownloadOptions(true)}>
-            <FontAwesomeIcon icon={faDownload} className="download-icon" />
-          </button>
 
           {showDownloadOptions && (
             <div className="modal-overlay">
@@ -264,7 +206,7 @@ function GeneratedRoadmap() {
               nodes={nodes}
               edges={edges}
               nodeTypes={{ custom: CustomNode }}
-              fit
+              fitView
               onInit={onInit}
             >
               <Background />
@@ -272,7 +214,7 @@ function GeneratedRoadmap() {
           </div>
 
           <div className="controls-container">
-            <button className="control-button" onClick={handleZoomIn}>
+          <button className="control-button" onClick={handleZoomIn}>
               <FontAwesomeIcon icon={faSearchPlus} />
             </button>
             <button className="control-button" onClick={handleZoomOut}>
@@ -283,7 +225,11 @@ function GeneratedRoadmap() {
             </button>
             <button className="control-button" onClick={handleShowModal}>
               <FontAwesomeIcon icon={faLink} />
-          </button>
+            </button>
+              {/* Botón de descarga */}
+              <button className="icon-button" onClick={() => setShowDownloadOptions(true)}>
+              <FontAwesomeIcon icon={faDownload} className="download-icon" />
+            </button>
           </div>
         </>
       )}
@@ -300,22 +246,6 @@ function GeneratedRoadmap() {
             <div className="modal-buttons">
               <button onClick={() => handleNewRoadmap()} className="modal-button">Sí 😃</button>
               <button onClick={closeModal} className="modal-button">No 🙁</button>
-            </div>
-          </div>
-        </div>
-      )}
-      {relatedTopicsModal && (
-        <div className="modal-overlay" onClick={closeModal}> 
-          <div className="modal" onClick={(e) => e.stopPropagation()}> 
-            <h1 className='modal-title'>¿Quieres crear una ruta de un tema relacionado? 🧐</h1>
-            <ul>
-              {relatedTopics.map((topic, index) => (
-                <li key={index}>{topic}</li>
-              ))}
-            </ul>
-            <div className="modal-buttons">
-              <button onClick={() => handleNewRoadmap()} className='modal-button'>Sí 😃</button>
-              <button onClick={closeModal} className='modal-button'>No 🙁</button> 
             </div>
           </div>
         </div>
